2803116009f135c03ba98ce1bff2f1bc7dd3d98b
Paulo Guilherme <paulopgss@gmail.com>
2020-08-08 14:34:46 -0300

    Update TeacherList

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,125 +1,121 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text } from 'react-native'
-import { Feather } from '@expo/vector-icons'
-import AsyncStorage from '@react-native-community/async-storage'
+import React, {useEffect, useState} from "react";
+import {View, ScrollView, Text, TextInput } from "react-native";
 
-import style from './styles'
-import PageHeader from '../../components/PageHeader'
-import TeacherItem, { Teacher } from '../../components/TeacherItem'
-import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler'
-import styles from './styles'
-import api from '../../services/api'
-import { useFocusEffect } from '@react-navigation/native'
+import PageHeader from "../../components/PageHeader";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
+import { Feather } from '@expo/vector-icons';
+
+import styles from "./styles";
+import {BorderlessButton, RectButton} from "react-native-gesture-handler";
+import api from "../../services/api";
+import AsyncStorage from '@react-native-community/async-storage';
+import {useFocusEffect} from "@react-navigation/native";
 
 function TeacherList() {
-    const [teachers, setTeachers] = useState([])
-    const [favorites, setFavorites] = useState<number[]>([])
-    const  [isFilterVisible, setIsFilterVisible] = useState(false)
 
-    const [subject, setSubject] = useState('')
-    const [week_day, setWeekDay] = useState('')
-    const [time, setTime] = useState('')
+    const [isfiltersVisible, setIsFiltersVisible] = useState(false);
+    const [favorites, setFavorites] = useState<number[]>([]);
+    const [teachers, setTeachers] = useState([]);
 
-    function loadFavorites() {
-        AsyncStorage.getItem('favorites').then(res => {
-            if (res) {
-                const favoritedTeachers = JSON.parse(res)
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    function loadFavorites (){
+        AsyncStorage.getItem('favorites').then(response => {
+            if(response) {
+                const favoritedTeachers = JSON.parse(response);
                 const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
-                    return teacher.id
-                })
-
-                setFavorites(favoritedTeachersIds)
+                    return teacher.id;
+                });
+                setFavorites(favoritedTeachersIds);
             }
-        })
+        });
     }
 
     useFocusEffect(() => {
-        loadFavorites()
-    })
+        loadFavorites();
+    });
 
-    function handleToggleFilterVisible() {
-        setIsFilterVisible(!isFilterVisible)
+    function handleToggleFiltersVisible() {
+        setIsFiltersVisible(!isfiltersVisible);
     }
 
-    async function handleFilterSubimit() {
-        loadFavorites()
-        const res = await api.get('classes', {
+    async function handleFiltersSubmit() {
+        loadFavorites();
+
+        const response = await api.get('classes', {
             params: {
                 subject,
                 week_day,
                 time
             }
-        })
-        setIsFilterVisible(false)
-        setTeachers(res.data)
+        });
+
+        setIsFiltersVisible(false);
+        setTeachers(response.data);
     }
-
     return (
-    <View style={style.container}>
-        <PageHeader 
-            title="Proffys disponíveis" 
-            headerRight={(
-                <BorderlessButton onPress={handleToggleFilterVisible}>
-                    <Feather name="filter" size={20} color="#FFF" />
-                </BorderlessButton>
-            )} >
-            { isFilterVisible && (
-                <View style={styles.searchForm}>
-                    <Text style={styles.label}>Matéria</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={subject}
-                        onChangeText={text => setSubject(text)}
-                        placeholder="Qual a matéria?"
-                        placeholderTextColor="#c1bccc"
-                    />
+        <View style={styles.container}>
+            <PageHeader title="Proffys disponíveis"
+                        headerRight={
+                            <BorderlessButton onPress={handleToggleFiltersVisible}>
+                                <Feather name="filter" size={20} color="#FFF"/>
+                            </BorderlessButton>
+                        }
+            >
+                { isfiltersVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Matéria</Text>
 
-                    <View style={styles.inputGroup}>
-                        <View style={styles.inputBlock}>
-                        <Text style={styles.label}>Dia da semana</Text>
-                        <TextInput
-                            style={styles.input}
-                            value={week_day}
-                            onChangeText={text => setWeekDay(text)}
-                            placeholder="Dia o dia?"
-                            placeholderTextColor="#c1bccc"
+                        <TextInput placeholderTextColor="#c1bccc"
+                                   style={styles.input}
+                                   placeholder="Qual a matéria"
+                                   value={subject}
+                                   onChangeText={text => setSubject(text)}
                         />
-                        </View>
 
-                        <View style={styles.inputBlock}>
-                        <Text style={styles.label}>Horário</Text>
-                        <TextInput
-                            style={styles.input}
-                            value={time}
-                            onChangeText={text => setTime(text)}
-                            placeholder="Qual horário"
-                            placeholderTextColor="#c1bccc"
-                        />
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Deia da semana</Text>
+                                <TextInput placeholderTextColor="#c1bccc"
+                                           style={styles.input}
+                                           placeholder="Qual o dia?"
+                                           value={week_day}
+                                           onChangeText={text => setWeekDay(text)}
+                                />
+                            </View>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Horário</Text>
+                                <TextInput placeholderTextColor="#c1bccc"
+                                           style={styles.input}
+                                           placeholder="Qual horário?"
+                                           value={time}
+                                           onChangeText={text => setTime(text)}
+                                />
+                            </View>
                         </View>
-                        
+                        <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
+                            <Text style={styles.submitButtonText}>Filtrar</Text>
+                        </RectButton>
                     </View>
-                    <RectButton onPress={handleFilterSubimit} style={styles.submitButton}>
-                        <Text style={styles.submitButtonText}>Filtrar</Text>
-                    </RectButton>
-                </View>
-            )}
-        </PageHeader>
-
-        <ScrollView 
-            style={styles.teacherList}
-            contentContainerStyle={{
+                )}
+            </PageHeader>
+            <ScrollView style={styles.teacherList} contentContainerStyle={{
                 paddingHorizontal: 16,
-                paddingBottom: 16
-            }} >
+                paddingBottom: 16,
+            }}>
                 {teachers.map((teacher: Teacher) => {
-                    return 
-                    <TeacherItem 
-                        key={teacher.id} 
-                        teacher={teacher}
-                        favorited={favorites.includes(teacher.id)} />
+                    return (
+                        <TeacherItem 
+                            key={teacher.id} 
+                            teacher={teacher} 
+                            favorited={favorites.includes(teacher.id)}/>
+                    );
                 })}
-        </ScrollView>
-    </View>
-)}
+            </ScrollView>
+        </View>
+    );
+}
 
-export default TeacherList
+export default TeacherList;
